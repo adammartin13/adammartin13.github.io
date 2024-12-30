ebcc5ece35ade2d7531145712abf2d9cae0cf658
adammartin13 <christopheradammartin13@gmail.com>
2024-12-30 16:57:35 -0600

    Added modal support for photos

--- js/scripts.js
+++ js/scripts.js
@@ -51,3 +51,22 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("banner");
+
+// Get the <span> element that closes the modal
+var close = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+  modal.style.display = "none";
+}
